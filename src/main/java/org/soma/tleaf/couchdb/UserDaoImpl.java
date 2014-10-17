1326b4cfa7459d2036319c42fd31870b81f111ac
System Administrator <root@subins-MacBook-Pro.local>
2014-10-17 14:16:01 +0900

    UserDao completed. userLogin, userSignUp, userSignOut

--- src/main/java/org/soma/tleaf/couchdb/UserDaoImpl.java
+++ src/main/java/org/soma/tleaf/couchdb/UserDaoImpl.java
@@ -3,6 +3,7 @@ package org.soma.tleaf.couchdb;
 import javax.inject.Inject;
 
 import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
 import org.ektorp.DocumentNotFoundException;
 import org.soma.tleaf.domain.HashId;
 import org.soma.tleaf.domain.UserInfo;
@@ -14,16 +15,14 @@ public class UserDaoImpl implements UserDao {
 	
 	private CouchDbConnector couchDbConnector_hashid;
 	private CouchDbConnector couchDbConnector_users;
+	private CouchDbInstance couchDbInstance;
 
-	@Override
-	public String userSignUp(String email, String password) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * 2014.10.15
+	 */
 	@Override
 	public String userLogin(String email, String password) {
-		// TODO Auto-generated method stub
+		// 1. Create User Database  2. Create User HashKey  3. Create UserInfo Document in User DB
 
 		HashId hashId;
 		
@@ -58,6 +57,101 @@ public class UserDaoImpl implements UserDao {
 		}
 
 		return "Your Password is Wrong";
+	}
+	
+	/**
+	 * 2014.10.16
+	 */
+
+	@Override
+	public String userSignUp(String email, String pw, String nickname,
+			String gender, Integer age) {
+
+		// 1. Create User Database  2. Create User HashKey  3. Create UserInfo Document in User DB
+		
+		try {
+
+			couchDbConnector_hashid = couchDbConn
+					.getCouchDbConnetor("tleaf_hashid");
+			
+			couchDbConnector_users = couchDbConn
+					.getCouchDbConnetor("tleaf_users");
+			
+			couchDbInstance = couchDbConn.getCouchDbInstance();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Failed to Connect to User Database";
+		}
+		
+		UserInfo userInfo = new UserInfo();
+		userInfo.setEmail(email); userInfo.setGender(gender);
+		userInfo.setNickname(nickname); userInfo.setAge(age);
+		userInfo.setPassword(pw);
+		
+		couchDbConnector_users.create( userInfo );
+		// create userinfo data
+		// couchDb Automatically gives out _id, _rev to userInfo
+		
+		System.out.println( userInfo.getHashId() );
+		System.out.println( userInfo.getRev() );
+		
+		HashId hashId = new HashId();
+		hashId.setEmail( email ); hashId.setHashId( userInfo.getHashId() );
+
+		couchDbConnector_hashid.create( hashId );
+		// makes mapping on email and user hashid
+		
+		couchDbInstance.createDatabase( "a" + userInfo.getHashId() );
+		// because a database name should start with an letter
+		
+		return "SignUp Successful";
+	}
+
+	@Override
+	public String userSignOut(String email, String pw) {
+		// TODO Auto-generated method stub
+		// 1. Check if email&pw is Correct.  2. if Correct, delete Hashid, Database, UserInfo
+		
+		// 1. check Account Info
+		if ( !userLogin( email, pw ).equals("Sucessfully Logged in") ) return "Account Info is Wrong";
+		
+		try {
+			
+			couchDbConnector_hashid = couchDbConn
+					.getCouchDbConnetor("tleaf_hashid");
+			
+			couchDbConnector_users = couchDbConn
+					.getCouchDbConnetor("tleaf_users");
+			
+			couchDbInstance = couchDbConn.getCouchDbInstance();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Failed to Connect to User Database";
+		}
+		
+		HashId hashId = new HashId();
+		UserInfo userInfo = new UserInfo();
+		
+		// 2. delete user data.
+		try {
+			
+			hashId = couchDbConnector_hashid.get( HashId.class, email );
+			couchDbConnector_hashid.delete( hashId.getEmail(), hashId.getRev() );
+			
+			userInfo = couchDbConnector_users.get( UserInfo.class, hashId.getHashId() );
+			couchDbConnector_users.delete( userInfo.getHashId(), userInfo.getRev() );
+			
+			couchDbInstance.deleteDatabase( "a" + userInfo.getHashId() );
+			
+			
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			return "Failed to Delete User Data";
+		}
+		
+		return "Completely deleted User data";
 	}
 
 }

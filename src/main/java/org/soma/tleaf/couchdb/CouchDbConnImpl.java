3ef4077df8017829be61f4e19a1da54d0eeb3faf
System Administrator <root@subins-MacBook-Pro.local>
2014-10-23 16:00:33 +0900

    Changed to a better solution about CouchDbConnImpl

--- src/main/java/org/soma/tleaf/couchdb/CouchDbConnImpl.java
+++ src/main/java/org/soma/tleaf/couchdb/CouchDbConnImpl.java
@@ -18,61 +18,84 @@ import org.springframework.stereotype.Repository;
 
 @Repository("couchDbConn")
 public class CouchDbConnImpl implements CouchDbConn {
-    /* properties file을 사용할때 쓴다. */
-    @Inject
-    private Environment environment;
-    
-    private CouchDbInstance couchDbInstance = null;
-    private HashMap<String, CouchDbConnector> couchDbConnectorHashMap = new HashMap<String, CouchDbConnector>();
+	/* properties file을 사용할때 쓴다. */
+	@Inject
+	private Environment environment;
 
-    /**
-     * 기존 소스코드 
-     */
-    @Override
-    public CouchDbInstance getCouchDbInstance() throws Exception {
-        StdHttpClient httpClient = null;
-        try {
+	private CouchDbInstance couchDbInstance = null;
+	private HashMap<String, CouchDbConnector> couchDbConnectorHashMap = new HashMap<String, CouchDbConnector>();
 
-            httpClient = (StdHttpClient) new StdHttpClient.Builder()
-                    .username(environment.getProperty("id"))
-                    .password(environment.getProperty("password"))
-                    .url(environment.getProperty("url"))
-                    .connectionTimeout(100000)
-                    .build();
-        } catch (MalformedURLException e) {
-            throw new Exception("Invalid URL" + environment.getProperty("url"));
-        }
+	/**
+	 * @author susu
+	 * Date Oct 22, 2014 4:34:47 PM
+	 * @return Existing Instance if made before, new Instance if first calling
+	 * @throws Exception
+	 */
+	@Override
+	public synchronized CouchDbInstance getCouchDbInstance() throws Exception {
 
-        CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
-        return dbInstance;
-    }
-    
-    private static final Logger logger = LoggerFactory.getLogger(CouchDbConnImpl.class);
-    
-    public synchronized CouchDbConnector getCouchDbConnetor(String dbName) throws Exception {
-    	
-    	if ( !couchDbConnectorHashMap.containsKey(dbName) ) {
-    		
-    		logger.info("Doesn't have " + dbName + " creating connector");
-    		
-    		getCouchDbInstance();
-        	if ( !couchDbInstance.checkIfDbExists(new DbPath(dbName)) ) couchDbInstance.createDatabase(dbName);
-        	couchDbConnectorHashMap.put(dbName, new StdCouchDbConnector(dbName, couchDbInstance) );
-        	
-        	return couchDbConnectorHashMap.get(dbName);
-    		
-    	}
-    	
-    	logger.info("Already have " + dbName + " returning existing CouchDbConnector");
-    	
-    	return couchDbConnectorHashMap.get(dbName);
-    	
-    }
-    
-  @Override
-  public String toString() {
-      return "CouchDbConnImpl [url=" + environment.getProperty("url") + ", id=" + environment.getProperty("id") + ", password="
-              + environment.getProperty("password") + "]";
-  }
+		if ( couchDbInstance == null ) {
+			couchDbInstance = createCouchDbInstance ();
+		}
+
+		return couchDbInstance;
+	}
+
+	private CouchDbInstance createCouchDbInstance () throws Exception {
+
+		StdHttpClient httpClient = null;
+		try {
+
+			httpClient = (StdHttpClient) new StdHttpClient.Builder()
+			.username(environment.getProperty("id"))
+			.password(environment.getProperty("password"))
+			.url(environment.getProperty("url"))
+			.connectionTimeout(100000)
+			.build();
+		} catch (MalformedURLException e) {
+			throw new Exception("Invalid URL" + environment.getProperty("url"));
+		}
+
+		return new StdCouchDbInstance( httpClient );
+
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(CouchDbConnImpl.class);
+
+	/**
+	 * @author susu
+	 * Date Oct 22, 2014 4:34:47 PM
+	 * @return Existing Connector if made before, new Connector if first calling
+	 * @throws Exception
+	 */
+	@Override
+	public synchronized CouchDbConnector getCouchDbConnetor(String dbName) throws Exception {
+
+		if ( !couchDbConnectorHashMap.containsKey(dbName) ) {
+
+			logger.info("Doesn't have " + dbName + " creating connector");
+
+			getCouchDbInstance();
+			
+			CouchDbConnector couchDbConnector = new StdCouchDbConnector(dbName, couchDbInstance);
+			couchDbConnector.createDatabaseIfNotExists();
+			
+			couchDbConnectorHashMap.put(dbName, couchDbConnector );
+
+			return couchDbConnectorHashMap.get(dbName);
+
+		}
+
+		logger.info("Already have " + dbName + " returning existing CouchDbConnector");
+
+		return couchDbConnectorHashMap.get(dbName);
+
+	}
+
+	@Override
+	public String toString() {
+		return "CouchDbConnImpl [url=" + environment.getProperty("url") + ", id=" + environment.getProperty("id") + ", password="
+				+ environment.getProperty("password") + "]";
+	}
 
 }

b6307d4a0c215b8045ef7739cc2b37ad893d4527
DevSusu <os1742@gmail.com>
2014-11-03 21:06:59 +0900

    Modified Javadoc Comments for API Opening

--- src/main/java/org/soma/tleaf/accesskey/AccessKeyManager.java
+++ src/main/java/org/soma/tleaf/accesskey/AccessKeyManager.java
@@ -10,6 +10,17 @@ import org.soma.tleaf.exception.InvalidAccessKeyException;
  */
 public interface AccessKeyManager {
 	
+	/** Checks If the Given AccessKey and UserId, AppId Matches. Also checks if the Token Expired at the Time
+	 * 
+	 * @author susu
+	 * Date Nov 3, 2014 4:27:12 PM
+	 * @param accessKey
+	 * @param appId
+	 * @param userId
+	 * @return boolean True if Valid and Usable, False if inVaild and Unusable
+	 * @throws InvalidAccessKeyException If AccessKey doesn't exist
+	 * @throws DatabaseConnectionException If Failed to Connect to the Database
+	 */
 	public boolean isAccessKeyValid ( String accessKey, String appId, String userId ) throws InvalidAccessKeyException,DatabaseConnectionException;
 	
 	// Makes access Keys. If String is in wrong format, it returns an invalid accessKey	
@@ -17,6 +28,18 @@ public interface AccessKeyManager {
 	public AccessKey createAccessKey ( String userId, String vaildFrom, String vaildTo, boolean isValid, String appId ) throws DatabaseConnectionException ;
 	public AccessKey createAccessKey ( String userId, String vaildFrom, Long vaildForMillis, boolean isValid, String appId ) throws DatabaseConnectionException ;
 	public AccessKey createAccessKey ( String userId, String vaildTo, boolean isValid, String appId ) throws DatabaseConnectionException ;
+	
+	/** Creates an Access Token Valid from the time of the Method Call, Lasting For given Milliseconds
+	 * 
+	 * @author susu
+	 * Date Nov 3, 2014 4:29:11 PM
+	 * @param userId
+	 * @param vaildForMillis
+	 * @param isValid
+	 * @param appId
+	 * @return AccessKey AccessKey Object
+	 * @throws DatabaseConnectionException
+	 */
 	public AccessKey createAccessKey ( String userId, Long vaildForMillis, boolean isValid, String appId ) throws DatabaseConnectionException ;
 
 }
--- src/main/java/org/soma/tleaf/couchdb/UserDao.java
+++ src/main/java/org/soma/tleaf/couchdb/UserDao.java
@@ -1,13 +1,41 @@
 package org.soma.tleaf.couchdb;
 
 import org.soma.tleaf.accesskey.AccessKey;
+import org.soma.tleaf.domain.UserInfo;
 import org.soma.tleaf.exception.CustomException;
 
 public interface UserDao {
 	
-	public String userSignUp( String email, String pw, String nickname, String gender, Integer age  ) throws CustomException;
+	/** Creates user Database, User HashId
+	 * 
+	 * @author susu
+	 * Date Nov 3, 2014 3:52:12 PM
+	 * @param userInfo
+	 * @return JSON String about the SignUp Process 
+	 * @throws CustomException If Email Already Exists or Failed to Connect to Database
+	 */
+	public String userSignUp( UserInfo userInfo ) throws CustomException;
+	
+	/** Creates an Access Token for the Specific User
+	 * 
+	 * @author susu
+	 * Date Nov 3, 2014 4:07:45 PM
+	 * @param email String ( full email )
+	 * @param password String ( full password )
+	 * @return AccessKey AccessKey Object of the Specific User.
+	 * @throws CustomException If Authentication Info is Wrong or Failed to Connect to Database
+	 */
 	public AccessKey userLogin( String email, String password ) throws CustomException;
-	public String userSignOut( String email, String pw ) throws CustomException;
+	
+	/** Deletes User Database, UserInfo Data
+	 * 
+	 * @author susu
+	 * Date Nov 3, 2014 4:10:17 PM
+	 * @param email String ( full email )
+	 * @param password String ( full password )
+	 * @return JSON String about the SignUp Process 
+	 * @throws CustomException If Authentication Info is Wrong or Failed to Connect to Database
+	 */
+	public String userSignOut( String email, String password ) throws CustomException;
 
-	// Giving out Cookies
 }
